3f4507e4f5770eceb39158fa32f64bfe2cc34085
BetterCallAle <dap22091998@gmail.com>
2023-03-16 14:40:46 +0100

    Exercise completed

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,13 +1,18 @@
 package org.lessons.java.christmas;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         List <String> christmasList = new ArrayList<>();
+
+        System.out.print("Inserisci il tuo nome? ");
+        String userName = scan.nextLine();
+
+        System.out.print("Inserisci il tuo indirizzo: ");
+        String userAddress = scan.nextLine();
+
         boolean stopCounter = false;
 
         while (!stopCounter){
@@ -24,7 +29,16 @@ public class Main {
             }
         }
 
+        Collections.sort(christmasList);
+
         System.out.println("I tuoi desideri sono: " + christmasList);
+
+        try{
+            ChristmasLetter christmasLetter = new ChristmasLetter(userName, userAddress, christmasList);
+            christmasLetter.send();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
 
         scan.close();
     }
